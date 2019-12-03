37df07b93408419cea87f0dcd28ff01d9b3d3fb3
Robert Luczynski <sabor@Roberts-iMac.local>
2019-12-03 03:31:54 +0100

    work on visual aspect

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  sizelg: number = 6;
+  sizemd: number = 6;
+  sizesm: number = 12;
+  sizexs: number = 12;
 
-  constructor() {}
+  constructor() { }
 
 }
