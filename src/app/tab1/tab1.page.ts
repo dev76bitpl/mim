651eb720c0bbef9a4be82f0d8e3fa16f4a2ab7fe
Robert Luczynski <sabor@Roberts-iMac.local>
2019-12-05 02:25:04 +0100

    dodawanie do localstorage

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,7 +10,107 @@ export class Tab1Page {
   sizemd: number = 6;
   sizesm: number = 12;
   sizexs: number = 12;
+  type: string;
+  userData = {};
+  data: any;
 
-  constructor() { }
+  public point = {
+    moc: 0,
+    zloto: 0,
+    sila: 12,
+    wytrzymalosc: 0
+  }
+
+  public feature = {
+    moc: "moc",
+    zloto: "złoto",
+    sila: "siła",
+    wytrzymalosc: "wytrzymałość"
+  }
+
+  constructor() {
+    this.updatePoints();
+    this.data = this.getUserData();
+    console.log("this.data.user", this.data.poszukiwacz);
+  }
+
+  pointPicker(i: number, type: string) {
+    switch (type) {
+      case "moc":
+        this.point.moc = this.point.moc + i;
+        console.log("this.point.moc: ", this.point.moc);
+        break;
+      case "sila":
+        this.point.sila = this.point.sila + i;
+        console.log("this.point.sila: ", this.point.sila);
+        break;
+      case "wytrzymalosc":
+        this.point.wytrzymalosc = this.point.wytrzymalosc + i
+        console.log("this.point.wytrzymalosc: ", this.point.wytrzymalosc);
+        break;
+      case "zloto":
+        this.point.zloto = this.point.zloto + i
+        console.log("this.point.zloto: ", this.point.zloto);
+        break;
+      default:
+        console.log("default type: ", type);
+        break;
+    }
+  }
+
+  test() {
+    var employees = [
+      {
+        "id": "1",
+        "firstName": "Tom",
+        "lastName": "Cruise"
+      },
+      {
+        "id": "2",
+        "firstName": "Maria",
+        "lastName": "Sharapova"
+      },
+      {
+        "id": "3",
+        "firstName": "James",
+        "lastName": "Bond"
+      }
+    ]
+
+    localStorage.setItem("myemps", JSON.stringify(employees));
+  }
+
+  updatePoints() {
+    var userCard = [
+      {
+        "uzytkownik": "sabor",
+        "poszukiwacz": "złodziej",
+        "charakter": "zły",
+        "start": "miasto",
+        "start_sila": 12,
+        "sila": 1,
+        "start_moc": 3,
+        "moc": 4,
+        "wytrzymalosc": 3,
+        "zloto": 23
+      }
+    ]
+
+    localStorage.setItem("userCard", JSON.stringify(userCard));
+  }
+
+  getUserData() {
+    const localData = localStorage.getItem('userCard');
+    try {
+      if (localData) {
+        this.userData = JSON.parse(localData);
+        //console.log('Data loaded', localData);
+        console.log('Data loaded', this.userData[0]);
+        return this.userData[0];
+      }
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
 
 }
